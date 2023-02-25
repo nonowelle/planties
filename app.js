dca22db36132fe92f6566bc140886cc003a658af
Nonowelle <menoel888@gmail.com>
2023-02-25 15:01:11 +0100

    connexion to mongo

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-
-
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 const express = require('express');
@@ -13,22 +11,21 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
-const methodOverride = require("method-override");
+const methodOverride = require('method-override');
 const moment = require('moment');
 const morgan = require('morgan');
-const ejsMate = require("ejs-mate");
+const ejsMate = require('ejs-mate');
 const session = require('express-session');
-
 
 const multer = require('multer');
 const { storage } = require('./cloudinary');
 const upload = multer({ storage });
-const { cloudinary } = require("./cloudinary");
+const { cloudinary } = require('./cloudinary');
 
-const ExpressError = require("./utils/ExpressError");
+const ExpressError = require('./utils/ExpressError');
 const { plantSchema, commentSchema, journalSchema } = require('./schemas.js');
 
-const Plant = require("./models/myPlants");
+const Plant = require('./models/myPlants');
 const Comment = require('./models/comment');
 const User = require('./models/user');
 const helmet = require('helmet');
@@ -38,26 +35,26 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const plantsRoutes = require('./routes/plants');
 const commentsRoutes = require('./routes/comments');
-const MongoDBStore = require("connect-mongo")(session);
+const MongoDBStore = require('connect-mongo')(session);
 const { contentSecurityPolicy } = require('helmet');
 // const { MongoStore } = require('connect-mongo');
 
 const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/plants';
 
-mongoose.connect(dbURL, {
+mongoose
+  .connect(dbURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-})
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!")
-        console.log(err)
-    });
-
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!!!');
+  })
+  .catch((err) => {
+    console.log('OH NO MONGO CONNECTION ERROR!!');
+    console.log(err);
+  });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -68,67 +65,66 @@ app.use(mongoSanitize());
 app.use(helmet());
 
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com",
-    "https://api.tiles.mapbox.com",
-    "https://kit.fontawesome.com",
-    "https://cdnjs.cloudflare.com",
-    "https://cdn.jsdelivr.net",
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.tiles.mapbox.com',
+  'https://kit.fontawesome.com',
+  'https://cdnjs.cloudflare.com',
+  'https://cdn.jsdelivr.net',
 ];
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com",
-    "https://stackpath.bootstrapcdn.com",
-    "https://fonts.googleapis.com",
-    "https://use.fontawesome.com",
+  'https://kit-free.fontawesome.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://fonts.googleapis.com',
+  'https://use.fontawesome.com',
 ];
 const connectSrcUrls = [];
-const fontSrcUrls = ["https://use.fontawesome.com"];
+const fontSrcUrls = ['https://use.fontawesome.com'];
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            childSrc: ["blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dkce7hoxj/",
-                "https://images.unsplash.com",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      childSrc: ['blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        'https://res.cloudinary.com/dkce7hoxj/',
+        'https://images.unsplash.com',
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
 );
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 const store = new MongoDBStore({
-    url: dbURL,
-    secret,
-    touchAfter: 24 * 60 * 60 //in seconds
+  url: dbURL,
+  secret,
+  touchAfter: 24 * 60 * 60, //in seconds
 });
-store.on("error", function (e) {
-    console.log('SESSION STORE ERROR', e)
-})
+store.on('error', function (e) {
+  console.log('SESSION STORE ERROR', e);
+});
 
 const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
+  store,
+  name: 'session',
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
 app.use(session(sessionConfig));
 app.use(flash());
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -139,12 +135,12 @@ passport.deserializeUser(User.deserializeUser());
 
 //FLASH MIDLLEWARE AND NAVBAR USER LOGGED IN (all templates will have access to these)
 app.use((req, res, next) => {
-    console.log(req.query);
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  console.log(req.query);
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 //Route Handlers
 app.use('/', userRoutes);
@@ -155,25 +151,23 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-
-
 //HOME
-app.get("/", (req, res) => {
-    res.render('home');
-})
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 //404 Route
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page not found', 404))
-})
+  next(new ExpressError('Page not found', 404));
+});
 
 //ERROR HANDLER
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
-    res.status(statusCode).render('notFound', { err });
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+  res.status(statusCode).render('notFound', { err });
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serving on port ${port}!!"`);
+  console.log(`Serving on port ${port}!!"`);
 });
