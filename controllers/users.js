74c69a5a0dcf4b8ca602bd2549b14c6f708194ce
Nonowelle <menoel888@gmail.com>
2023-04-07 14:43:47 +0200

    fix session problem

--- controllers/users.js
+++ controllers/users.js
@@ -2,45 +2,49 @@ const User = require('../models/user');
 const Plant = require('../models/myPlants');
 
 module.exports.registerForm = (req, res) => {
-    res.render('users/register')
-}
+  res.render('users/register');
+};
 
 module.exports.newUser = async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Planties!');
-            res.redirect('/plants');
-        })
-
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash('success', 'Welcome to Planties!');
+      res.redirect('/plants');
+    });
+  } catch (e) {
+    req.flash('error', e.message);
+    res.redirect('register');
+  }
+};
 
 module.exports.loginForm = (req, res) => {
-    res.render('users/login')
-}
+  res.render('users/login');
+};
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome back!');
-    const redirectURL = req.session.returnTo || '/plants';
-    delete req.session.returnTo;
-    res.redirect(redirectURL);
-}
+  req.flash('success', 'Welcome back!');
+  const redirectURL = req.session.returnTo || '/plants';
+  delete req.session.returnTo;
+  console.log('ICI', req.session);
+  res.redirect(redirectURL);
+};
 
 module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash('success', 'Goodbye!');
-    res.redirect('/')
-}
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+  req.flash('success', 'Goodbye!');
+  res.redirect('/');
+};
 
 module.exports.myCollection = async (req, res) => {
-    const usersPlants = await Plant.find({ owner: req.user });
-    console.log(usersPlants);
-    res.render('users/userIndex', { usersPlants, family: 'My Collection' });
-}
+  const usersPlants = await Plant.find({ owner: req.user });
+  res.render('users/userIndex', { usersPlants, family: 'My Collection' });
+};
--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,25 @@ const passport = require('passport');
 const { isLoggedIn, isOwner } = require('../middleware');
 const users = require('../controllers/users');
 
-router.route('/register')
-    .get(users.registerForm)
-    .post(catchAsync(users.newUser))
+router
+  .route('/register')
+  .get(users.registerForm)
+  .post(catchAsync(users.newUser));
 
-router.route('/login')
-    .get(users.loginForm)
-    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+router
+  .route('/login')
+  .get(users.loginForm)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/login',
+    }),
+    users.login
+  );
 
-router.get('/logout', users.logout)
-
+router.get('/logout', users.logout);
 
 //MY COLLECTION
-router.get("/mycollection", isLoggedIn, catchAsync(users.myCollection));
-
+router.get('/mycollection', isLoggedIn, catchAsync(users.myCollection));
 
 module.exports = router;
